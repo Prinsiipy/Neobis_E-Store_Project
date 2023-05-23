983fe7abcf675795c1da8d732d7c34955d58f2c0
Prinsiipy <chomoevisa@gmail.com>
2023-05-24 00:08:07 +0600

    Initial commit

--- users/models.py
+++ users/models.py
@@ -1,28 +1,59 @@
-from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
-from django.contrib.auth.models import Group, Permission, PermissionsMixin
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class SuperUser(BaseUserManager):
+    def create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError("You need to fill in the field")
+        if not email:
+            raise ValueError("You need to fill in the field")
+        if not password:
+            raise ValueError("You need to come up with a password")
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password):
+
+        user = self.create_user(
+            username=username,
+            email=self.normalize_email(email),
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        related_name='user_groups'  # Измененный related_name
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        related_name='user_permissions'  # Измененный related_name
-    )
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    role = models.CharField(max_length=100, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
-class Cart(models.Model):
+    objects = SuperUser()
 
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='cart_user'  # Измененный related_name
-    )
+    def __str__(self):
+        return self.username
